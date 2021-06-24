332d0f3cee7e620ed8ff169e88ab9092dc71caa6
minpeter <kali2005611@gmail.com>
2021-06-24 00:11:44 +0000

    git log reader.py edit

--- lib/logreader.py
+++ lib/logreader.py
@@ -1,14 +1,16 @@
 import git
-import os
 import datetime
 
-def commitRead():
-    filename = "main.py" #임시
 
-    #path = os.path.abspath(__file__)
-    path = "/workspaces/instagram_github_contribution"
+def gitlog(path):
     repo = git.Repo(path)
+    commitlog = commitreader(repo)
+    filename = bestfilebrowser(path)
 
+    return commitlog, filename
+
+
+def commitreader(repo):
     commits = list(repo.iter_commits("main", max_count=3))
 
     commitlog = """"""
@@ -18,4 +20,14 @@ def commitRead():
         nd = datetime.datetime.now().date() #현제 날짜만
         if nd == dd:
             commitlog += str(i.message)
-    return filename, commitlog
+    
+    return commitlog
+
+def bestfilebrowser(path):
+    g = git.Git(path)
+    loginfo=g.log("--pretty=format:","--name-only --since '1 month ago'")
+    filename = "main.py"
+    print(loginfo)
+    return filename
+
+bestfilebrowser("/workspaces/instagram_github_contribution")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from lib import logreader, fileopener, imaging, upload
+import os
 
-filename, commitlog = logreader.commitRead()
+path = os.getcwd()
+filename, commitlog = logreader.gitlog(path)
 msg = fileopener.fileopen(filename)
 imaging.imageMaker(msg)
 upload.upLoad(commitlog)
